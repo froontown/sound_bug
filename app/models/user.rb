4b8a016067e28cab3ede485a64e0b9686b940dd7
froontown <froontown@froontowns-MacBook-Pro.local>
2017-01-30 13:08:15 -0500

    Created Users controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
