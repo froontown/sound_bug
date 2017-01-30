e22af2fbdb1ee3ed9c2a35f7f40f0a2171248ddf
froontown <froontown@froontowns-MacBook-Pro.local>
2017-01-30 13:49:40 -0500

    Re-added validations to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
