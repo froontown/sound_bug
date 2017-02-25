5e322885a32f5d398b0988bd990891cbd6275141
froontown <froontown@froontowns-MacBook-Pro.local>
2017-02-25 17:48:29 -0500

    iddy biddy baby steps.

--- app/models/vote.rb
+++
@@ -1,6 +0,0 @@
-class Vote < ApplicationRecord
-  belongs_to :user
-  belongs_to :song
-
-  validates :value, presence: true
-end
--- db/migrate/20170208222418_add_score.rb
+++
@@ -1,5 +0,0 @@
-class AddScore < ActiveRecord::Migration[5.0]
-  def change
-    add_column :songs, :score, :integer, default: 0
-  end
-end
